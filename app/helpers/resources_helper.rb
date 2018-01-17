85e2e023118f8b1876f4a0580dc38f42d90153e9
Kainat <kainatsaeed10@gmail.com>
2018-01-17 19:33:24 +0500

    Fix video size

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,6 +1,7 @@
 module ResourcesHelper
 	def embed(youtube_url)
     youtube_id = youtube_url.split("=").last
-    content_tag(:iframe, :allowfullscreen , src: "//www.youtube.com/embed/#{youtube_id}")
+    content_tag(:iframe,'', src: "//www.youtube.com/embed/#{youtube_id}", width: 1100,
+   height: 500, frameborder: 5)
   end
 end
