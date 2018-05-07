15e288fcdae07579ecd9296d6c7b710cca4336a2
hussainakram <hussain.akram128@gmail.com>
2018-05-07 18:05:10 +0500

    show joined chats

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :chatroom_users
   has_many :users, through: :chatroom_users
   has_many :messages
+
+  def joined?(user)
+    self.users.include?(user)
+  end
 end
